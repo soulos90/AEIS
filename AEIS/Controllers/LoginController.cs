6f6d43c3ac90e1b36bf3ee0f206bbd0d6182df97
Joel Cahill <joelcahill@csus.edu>
2018-04-12 21:01:06 -0700

    added login functionality to LoginController

--- AEIS/Controllers/LoginController.cs
+++ AEIS/Controllers/LoginController.cs
@@ -25,5 +25,29 @@ namespace StateTemplateV5Beta.Controllers
             SController.Login(user.ID);
             return View("LoggedIn");
         }
+
+		[HttpPost]
+		public ActionResult LoginCheck(string userName, string password)
+		{
+			using (var context = new DBUContext())
+			{				
+					var getUser = (from s in context.Users where s.ID == userName select s).FirstOrDefault();
+					if (getUser != null)
+					{ 
+						var saltHash = getUser.PassSalt;
+						var encodedPassword = new UsersController().HashPassword(password, saltHash);
+
+						var query = (from s in context.Users where s.ID == userName && s.Passhash.Equals(encodedPassword) select s).FirstOrDefault();
+						if(query != null)
+						{
+							return View("LoggedIn");
+						}
+						ViewBag.ErrorMessage = "Invalid User Name or Password";
+						return View("LoginFail");
+					}
+					ViewBag.ErrorMessage = "Invalid User Name or Password";
+					return View("LoginFail");
+			}
+		}
     }
 }
