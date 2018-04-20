bd1832674a28857748030c5207d50f3075863714
Matthew Swart <matthewswart@csus.edu>
2018-04-20 10:45:50 -0700

    - Nav bar now reflects available options when logged in / out. - Homogenized spelling of organization to avoid later conflicts

---
+++ AEIS/Models/System.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using StateTemplateV5Beta.Controllers;
+
+namespace StateTemplateV5Beta.Models
+{
+    public class System
+    {
+        public string SystemName;
+        public int[] SectionScores { get; }
+
+        public System(string systemNum)
+        {
+            string userId = HomeController.active.GetID();
+            AnswersController aController = new AnswersController();
+            Environment e = new Environment();
+            
+            int numQuestions = int.Parse(Environment.NumQus);
+
+            for (int curQuestion = 1; curQuestion < numQuestions; curQuestion++)
+            {
+                
+            }
+        }
+    }
+}
