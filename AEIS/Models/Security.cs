4eedb96fd0da6f3ee5aef86085264b160adee900
Nathan Raymond <nathanraymond@csus.edu>
2018-04-27 03:57:02 -0700

    token is working now, pretty sure cookies still arn't but I don't care atm

--- AEIS/Models/Security.cs
+++ AEIS/Models/Security.cs
@@ -8,16 +8,19 @@ namespace StateTemplateV5Beta.Models
     public class Security
     {
         public string ID { get; set; }
-        public HttpCookie Cookie { get; set; }
         public bool IsLoggedIn { get; set; }
         public bool Remember { get; set; }
         public Security()
         {
             IsLoggedIn = false;
             ID = null;
-            Cookie = null;
             Remember = false;
         }
-
+        public Security(string id,bool log, bool rem)
+        {
+            ID = id;
+            Remember = rem;
+            IsLoggedIn = log;
+        }
     }
 }
